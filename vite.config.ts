6c59373288f3b347c3430c8d99df4ee61f8d3fb8
Reydv <117568719+Reydv@users.noreply.github.com>
2026-02-16 19:53:46 +0700

    chore(auth): refactor to use env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5244', // Your Backend Port from Postman
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_TARGET, // Fallback if .env is missing
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  }
 })
