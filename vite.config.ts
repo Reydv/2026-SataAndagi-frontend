f17d50f777169d2ba40f66060a3f2392e80b3882
Reydv <117568719+Reydv@users.noreply.github.com>
2026-02-16 19:48:21 +0700

    fix(auth): fix backend port typo

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,13 +7,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Catch any request that starts with "/api"
       '/api': {
-        target: 'http://localhost:5224', // Your backend port
+        target: 'http://localhost:5244', // Your Backend Port from Postman
         changeOrigin: true,
-        secure: false, // Set to false if you are using self-signed SSL (https) locally
-        // Optional: Remove "/api" from the path if your backend doesn't expect it
-        // rewrite: (path) => path.replace(/^\/api/, ''), 
+        secure: false,
       },
     },
   },
